d21c867e6a7d7e8f14bbe2967f7c11457d5f6f1b
Aidan <aidanmyers@me.com>
2021-03-05 12:35:52 -0500

    Create Player.java

---
+++ Project Folder/app/src/objects/Player.java
@@ -0,0 +1,42 @@
+import android.graphics.Color;
+
+public class Player(){
+    int playerID;
+    String playerName;
+    Color playerColor;
+    ArrayList<Integer> scores = new ArrayList<Integer>;
+}
+
+public int getPlayerID(){
+    return this.playerID;
+}
+
+public void setPlayerID(int ID){
+    this.playerID = ID;
+}
+
+public String getPlayerName(){
+    return this.playerName;
+}
+
+public void setPlayerName(String name){
+    this.playerName = name;
+}
+
+public Color getColor(){
+    return this.playerColor;
+}
+
+public void setColor(Color color){
+    this.playerColor = color;
+}
+
+public int getScore(int holeID){
+    return this.scores.get(holeID-1);
+}
+
+public void setScore(int holeID, int score){
+    if(scores.size() <= holeID)
+        scores.ensureCapacity(holeID);
+    this.scores.set(holeID-1, score);
+}
