8782cd45be15ef194d3b2cdf241de00037cc800a
Aidan <aidanmyers@me.com>
2021-03-05 12:48:01 -0500

    Update Player.java, Game.java

--- Project Folder/app/src/objects/Player.java
+++ Project Folder/app/src/objects/Player.java
@@ -1,42 +1,53 @@
 import android.graphics.Color;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class Player(){
-    int playerID;
-    String playerName;
-    Color playerColor;
-    ArrayList<Integer> scores = new ArrayList<Integer>;
-}
+class Player implements Serializable{
 
-public int getPlayerID(){
-    return this.playerID;
-}
+    public Player(){
+        int playerID;
+        String playerName;
+        Color playerColor;
+        ArrayList<Integer> scores = new ArrayList<Integer>;
+    }
 
-public void setPlayerID(int ID){
-    this.playerID = ID;
-}
+    public int getPlayerID(){
+        return this.playerID;
+    }
 
-public String getPlayerName(){
-    return this.playerName;
-}
+    public void setPlayerID(int ID){
+        this.playerID = ID;
+    }
 
-public void setPlayerName(String name){
-    this.playerName = name;
-}
+    public String getPlayerName(){
+        return this.playerName;
+    }
 
-public Color getColor(){
-    return this.playerColor;
-}
+    public void setPlayerName(String name){
+        this.playerName = name;
+    }
 
-public void setColor(Color color){
-    this.playerColor = color;
-}
+    public Color getColor(){
+        return this.playerColor;
+    }
 
-public int getScore(int holeID){
-    return this.scores.get(holeID-1);
-}
+    public void setColor(Color color){
+        this.playerColor = color;
+    }
 
-public void setScore(int holeID, int score){
-    if(scores.size() <= holeID)
-        scores.ensureCapacity(holeID);
-    this.scores.set(holeID-1, score);
+    public int getScore(int holeID){
+        return this.scores.get(holeID-1);
+    }
+
+    public void setScore(int holeID, int score){
+        if(scores.size() <= holeID)
+            scores.ensureCapacity(holeID);
+        this.scores.set(holeID-1, score);
+    }
+
+    private void readObject(){
+        
+    }
+
 }
