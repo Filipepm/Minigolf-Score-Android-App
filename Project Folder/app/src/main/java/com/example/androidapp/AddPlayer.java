57d8eb57ad2948eb88c1f9821f5e928b735156c0
sreekeshs@vcu.edu <sreekeshs@vcu.edu>
2021-03-05 19:08:47 -0500

    Code for choosing color of player

--- Project Folder/app/src/main/java/com/example/androidapp/AddPlayer.java
+++ Project Folder/app/src/main/java/com/example/androidapp/AddPlayer.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddPlayer extends AppCompatActivity {
 
-    private GridView colorGrid;
+    private GridView gridView;
     private EditText playerName;
     private Button SubmitButton;
 
@@ -17,14 +20,33 @@ public class AddPlayer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_player);
-        colorGrid = findViewById(R.id.ColorGrid);
-        playerName = findViewById(R.id.PlayerNameInput);
+
+        gridView = findViewById(R.id.gridview);
+        gridview.setAdapter(new ColorAdapter(this));
+
+        playerName = findViewById(R.id.PlayerNameInput); 
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id){ 
+                
+                Intent i = new Intent();
+                i.putExtra("id", position);
+                int position = i.getExtras().getInt("id");
+                ColorAdapter colorAdapter = new ColorAdapter(this);
+
+                
+                ImageView imageView = (ImageView) findViewById(R.id.ColorView);
+                final Color color = imageView.setImageResource(colorAdapter.colorIds[position]); 
+            }
+        });
+
+
 
         SubmitButton.setOnClickListener(new View.OnClickListener() {
 
             @Ovverride
             public void OnClick(View view) {
-                final String playerName = PlayerNameInput.getText().toString();
+                final String playerName = PlayerNameInput.getText().toString(); 
 
                 Player player = new Player();
             }
