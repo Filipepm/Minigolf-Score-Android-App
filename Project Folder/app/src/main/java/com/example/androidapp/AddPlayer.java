05d89ff2b5f65d80ccdc543d9852efe009080d5b
sreekeshs@vcu.edu <sreekeshs@vcu.edu>
2021-04-14 01:25:05 -0400

    Update NewPlayer.xml, Update AddPlayer.java
    Fixed new player form. Note that AddPlayer.java cannot access Player object.

--- Project Folder/app/src/main/java/com/example/androidapp/AddPlayer.java
+++ Project Folder/app/src/main/java/com/example/androidapp/AddPlayer.java
@@ -2,59 +2,75 @@ package com.example.androidapp;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.EditText;
-import android.widget.GridView;
-import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddPlayer extends AppCompatActivity {
 
-    private GridView gridView;
     private EditText playerName;
-    private Button SubmitButton;
+    private Button submit;
+    private RadioButton red, orange, yellow, green, blue, purple;
+    int playerIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_player);
 
-        gridView = findViewById(R.id.gridview);
-        gridView.setAdapter(new ColorAdapter(this));
+        playerName = (EditText) findViewById(R.id.NameInput);
+        red = (RadioButton) findViewById(R.id.redButton);
+        orange = (RadioButton) findViewById(R.id.orangeButton);
+        yellow = (RadioButton) findViewById(R.id.yellowButton);
+        green = (RadioButton) findViewById(R.id.greenButton);
+        blue = (RadioButton) findViewById(R.id.blueButton);
+        purple = (RadioButton) findViewById(R.id.purpleButton);
+        submit = (Button) findViewById(R.id.SubmitButton);
 
-        playerName = findViewById(R.id.PlayerNameInput); 
-
-        gridView.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id){ 
-                
-                Intent i = new Intent();
-                i.putExtra("id", position);
-                position = i.getExtras().getInt("id");
-                ColorAdapter colorAdapter = new ColorAdapter(this);
-
-                
-                ImageView imageView = (ImageView) findViewById(R.id.ColorView);
-                final Color color = imageView.setImageResource(colorAdapter.colorIds[position]);
-            }
-        });
-
-
-
-        SubmitButton.setOnClickListener(new View.OnClickListener() {
-
+        submit.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
-            public void OnClick(View view) {
-                final String playerName = PlayerNameInput.getText().toString(); 
+            public void onClick(View v) {
+                final String Name = playerName.getText().toString();
+                final Color playerColor = colorButtons();
 
-                Player player = new Player();
+                //Player player = new Player (++playerIndex, Name, playerColor);
+
+                Intent i = new Intent(AddPlayer.this, MainActivity.class);
+                startActivity(i);
             }
         });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private Color colorButtons() {
+       Color color = null;
+        if (red.isChecked()) {
+            color = Color.valueOf(238, 80, 46);
+        } else if (orange.isChecked()) {
+            color = Color.valueOf(238, 176, 46);
+        } else if (yellow.isChecked()) {
+            color = Color.valueOf(229, 220, 42);
+        } else if (green.isChecked()) {
+            color = Color.valueOf(51, 229, 42);
+        } else if (blue.isChecked()) {
+            color = Color.valueOf(42, 144, 229);
+        } else if (purple.isChecked()) {
+            color = Color.valueOf(112, 42, 29);
+        } else {
+            color = Color.valueOf(255, 255, 255); //WHITE
+        }
         
+        return color;
     }
 
 }
