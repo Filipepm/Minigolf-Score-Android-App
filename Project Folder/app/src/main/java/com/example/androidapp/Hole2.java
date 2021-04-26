4838c4aaee9beabc34f6a649c95ab0555d8e1364
sreekeshs@vcu.edu <sreekeshs@vcu.edu>
2021-04-26 14:09:31 -0400

    Revert "Commit the scorecard"
    This reverts commit d642eb6e13877a6104f22ab01df24a01e7b8b7a4.

---
+++ Project Folder/app/src/main/java/com/example/androidapp/Hole2.java
@@ -0,0 +1,81 @@
+package com.example.androidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Hole2 extends AppCompatActivity {
+
+    EditText hole1score, hole2score;
+    TextView hole1total, hole2total, holenumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hole2);
+
+        hole1score = (EditText) findViewById(R.id.hole1score);
+        hole1total = (TextView) findViewById(R.id.total1score);
+        hole2score = (EditText) findViewById(R.id.hole2score);
+        hole2total = (TextView) findViewById(R.id.total2score);
+        holenumber = (TextView) findViewById(R.id.holenumber);
+
+        hole1total.setText("2");
+        hole2total.setText("1");
+        holenumber.setText("2");
+
+        hole1score.addTextChangedListener(scoretext);
+        hole2score.addTextChangedListener(scoretext1);
+
+        Button btnPrev = findViewById(R.id.prev_hole);
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Hole2.this, MainActivity.class ));
+            }
+        });
+    }
+
+    private TextWatcher scoretext = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            hole1total.setText("4");
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private TextWatcher scoretext1 = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            hole2total.setText("3");
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+}
---
+++ Project Folder/app/src/main/java/com/example/androidapp/Player.java
@@ -0,0 +1,59 @@
+package com.example.androidapp;
+
+import android.graphics.Color;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Player implements Serializable{
+
+    int playerID;
+    String playerName;
+    Color playerColor;
+    ArrayList<Integer> scores;
+
+    public Player() {
+        scores = new ArrayList<Integer>();
+    }
+
+    public Player(int playerID, String playerName, Color playerColor) {
+        this.playerID = playerID;
+        this.playerName = playerName;
+        this.playerColor = playerColor;
+    }
+
+    public int getPlayerID(){
+        return this.playerID;
+    }
+
+    public void setPlayerID(int ID){
+        this.playerID = ID;
+    }
+
+    public String getPlayerName(){
+        return this.playerName;
+    }
+
+    public void setPlayerName(String name){
+        this.playerName = name;
+    }
+
+    public Color getColor(){
+        return this.playerColor;
+    }
+
+    public void setColor(Color color){
+        this.playerColor = color;
+    }
+
+    public int getScore(int holeID){
+        return this.scores.get(holeID-1);
+    }
+
+    public void setScore(int holeID, int score){
+        if(scores.size() <= holeID)
+            scores.ensureCapacity(holeID);
+        this.scores.set(holeID-1, score);
+    }
+}
