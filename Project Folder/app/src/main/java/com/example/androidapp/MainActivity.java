5dc35f1d59ca7e94a7f63cbb1aee52f9f29bdcc3
Filipepm <mouraofilipep@gmail.com>
2021-04-29 13:57:37 -0400

    Pushed the Scorecard

--- Project Folder/app/src/main/java/com/example/androidapp/MainActivity.java
+++ Project Folder/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -2,34 +2,58 @@ package com.example.androidapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
 public class MainActivity extends AppCompatActivity {
     EditText list;
-    int[] array = {2, 5, 6, 8, 0, 7, 9, 4};
+    TextView totalParView, list2, holeNumber;
+    Button nextHole, prevHole;
+    int holeNum = 0;
+    int totalPar = 0;
+    int[] array = new int[10];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game);
+        setContentView(R.layout.activity_main);
         list = findViewById(R.id.hole1score);
-        ArrayList<String> arrayList = new ArrayList<String>();
-        int listString = array[0];
-        list.setText(String.valueOf(listString));
+        list2 = findViewById(R.id.total2score);
+        nextHole = findViewById(R.id.next_hole);
+        prevHole = findViewById(R.id.prev_hole);
+        totalParView = findViewById(R.id.total1score);
+        holeNumber = findViewById(R.id.holenumber);
 
-        ImageButton btnAddPlayer = findViewById(R.id.add_player_btn2);
+        nextHole.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(holeNum < array.length - 1)
+                    holeNum = holeNum+1;
+                int listString = array[holeNum];
+                holeNumber.setText(String.valueOf(holeNum + 1));
+                list.setText(String.valueOf(listString));
+            }
+        });
+
+        prevHole.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(holeNum > 0)
+                    holeNum = holeNum-1;
+                int listString = array[holeNum];
+                holeNumber.setText(String.valueOf(holeNum + 1));
+                list.setText(String.valueOf(listString));
+            }
+        });
+
+        ImageButton btnAddPlayer = findViewById(R.id.add_player_btn);
         btnAddPlayer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,11 +61,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button btnNext = findViewById(R.id.next_hole);
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        list.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, Hole2.class));
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+                    int holeValue = Integer.valueOf(list.getText().toString());
+                    array[holeNum] = holeValue;
+                }
+                catch(NumberFormatException e){
+                }
+
+                for(int i = 0; i < array.length; i++) {
+                    totalPar = totalPar + array[i];
+                }
+                totalParView.setText(String.valueOf(totalPar));
+                totalPar = 0;
             }
         });
     }
